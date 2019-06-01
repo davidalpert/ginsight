92d247bbaa46c9cb446879c5ae6199c655c6d06b
David Alpert <david@spinthemoose.com>
2019-05-31 21:53:42 -0500

    port initial gingko test for schema methods

---
+++ insight/insight_suite_test.go
@@ -0,0 +1,64 @@
+package insight_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/jarcoal/httpmock"
+	"gopkg.in/resty.v1"
+
+	insight "github.com/davidalpert/ginsight/insight"
+)
+
+func TestInsight(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Insight Suite")
+}
+
+var MockBaseURL = "https://jira.mydomain.com"
+
+func MockURLFor(endpoint string) string {
+	return MockBaseURL + endpoint
+}
+
+var testClient *insight.Client
+
+var _ = BeforeSuite(func() {
+	// build the api client
+	clientConfiguration := insight.ClientConfiguration{
+		BaseURL:  MockBaseURL,
+		Username: "mal",
+		Password: "serenity",
+		Debug:    false, // toggle true to see Resty and other logs
+  }
+
+  if client, err := insight.BuildClient(&clientConfiguration); err == nil {
+    testClient = client 
+  }
+
+	// block all HTTP requests
+	httpmock.Activate()
+
+	// and wire up the httpmock transport to resty
+	httpmock.ActivateNonDefault(resty.DefaultClient.GetClient())
+})
+
+var _ = BeforeEach(func() {
+	// remove any mocks
+	httpmock.Reset()
+})
+
+var _ = AfterSuite(func() {
+	httpmock.DeactivateAndReset()
+})
+
+// all the Insight API responses speak application/json so we need
+// to feed that back to the resty library so that it knows how to
+// unmarshall the responses.
+func InsightApiResponder(statusCode int, body string) httpmock.Responder {
+	response := httpmock.NewStringResponse(statusCode, body)
+	response.Header.Add("content-type", "application/json")
+	return httpmock.ResponderFromResponse(response)
+}
---
+++ insight/object_schemas_test.go
@@ -0,0 +1,343 @@
+package insight_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/jarcoal/httpmock"
+
+	insight "github.com/davidalpert/ginsight/insight"
+)
+
+var _ = Describe("Client", func() {
+	Describe("ObjectSchemas", func() {
+		Describe("List All", func() {
+			endpoint := "/rest/insight/1.0/objectschema/list"
+
+			Context("With no schemas", func() {
+				fixture := `{
+          "objectschemas": []
+        }`
+
+				It("returns a slice of InsightObjectSchema", func() {
+					httpmock.RegisterResponder("GET", endpoint, InsightApiResponder(200, fixture))
+
+					schemaList, err := testClient.GetObjectSchemas()
+
+					Expect(err).To(BeNil())
+
+					var sliceOfObjectSchema *insight.ObjectSchemaList
+					Expect(schemaList).To(BeAssignableToTypeOf(sliceOfObjectSchema))
+				})
+
+				It("should be empty", func() {
+					httpmock.RegisterResponder("GET", endpoint, InsightApiResponder(200, fixture))
+
+					schemaList, err := testClient.GetObjectSchemas()
+
+					Expect(err).To(BeNil())
+					Expect(schemaList.Schemas).To(HaveLen(0))
+				})
+			})
+
+			Context("With 2 schemas", func() {
+				fixture := `
+          {
+            "objectschemas": [
+               {
+                  "id": 3,
+                  "name": "Hardware",
+                  "objectSchemaKey": "HARD",
+                  "status": "Ok",
+                  "created": "06/May/19 6:33 AM",
+                  "updated": "06/May/19 6:33 AM",
+                  "objectCount": 0,
+                  "objectTypeCount": 0
+               },
+               {
+                 "id": 5,
+                 "name": "Computers",
+                 "objectSchemaKey": "IT",
+                 "status": "Ok",
+                 "description": "The IT department schema",
+                 "created": "25/May/19 3:42 AM",
+                 "updated": "25/May/19 3:42 AM",
+                 "objectCount": 0,
+                 "objectTypeCount": 0
+              }
+            ]
+          }`
+
+				It("should deserialize to two schemas", func() {
+					httpmock.RegisterResponder("GET", endpoint, InsightApiResponder(200, fixture))
+
+					schemaList, err := testClient.GetObjectSchemas()
+
+					Expect(err).To(BeNil())
+					Expect(schemaList.Schemas).To(HaveLen(2))
+				})
+			})
+		})
+
+		Describe("Create one ObjectSchema", func() {
+			endpoint := "/rest/insight/1.0/objectschema/create"
+			schemaCreate := insight.ObjectSchemaCreateUpdateRequest{
+				Name:        "Computers",
+				Key:         "COMP",
+				Description: "The IT department schema",
+			}
+
+			Context("Successful", func() {
+				fixture := `{
+          "id": 5,
+          "name": "Computers",
+          "objectSchemaKey": "COMP",
+          "status": "Ok",
+          "description": "The IT department schema",
+          "created": "25/May/19 3:42 AM",
+          "updated": "25/May/19 3:42 AM",
+          "objectCount": 0,
+          "objectTypeCount": 0
+        }`
+
+				It("completes successfully", func() {
+					httpmock.RegisterResponder("POST", endpoint, InsightApiResponder(201, fixture))
+
+					schemaCreate := insight.ObjectSchemaCreateUpdateRequest{
+						Name:        "Computers",
+						Key:         "COMP",
+						Description: "The IT department schema",
+					}
+
+					newSchema, err := testClient.CreateSchema(&schemaCreate)
+
+					Expect(err).To(BeNil())
+					Expect(newSchema).To(Not(BeNil()))
+					Expect(newSchema.Name).To(Equal(schemaCreate.Name))
+					Expect(newSchema.Key).To(Equal(schemaCreate.Key))
+					Expect(newSchema.Description).To(Equal(schemaCreate.Description))
+				})
+			})
+
+			Context("Name already exists", func() {
+				fixture := `{
+          "errorMessages": [],
+          "errors": {
+            "name": "Name has to be unique!"
+          }
+        }`
+
+				It("returns an error", func() {
+					httpmock.RegisterResponder("POST", endpoint, InsightApiResponder(400, fixture))
+
+					_, err := testClient.CreateSchema(&schemaCreate)
+
+					Expect(err).To(Not(BeNil()))
+					clientError := insight.ClientError{}
+					Expect(err).To(BeAssignableToTypeOf(&clientError))
+					Expect(err.Error()).To(Equal(fixture))
+				})
+			})
+		})
+
+		Describe("Get one ObjectSchema", func() {
+			endpoint := "/rest/insight/1.0/objectschema/5"
+
+			Context("Successful", func() {
+				fixture := `{
+          "id": 5,
+          "name": "Computers",
+          "objectSchemaKey": "IT",
+          "status": "Ok",
+          "description": "The IT department schema",
+          "created": "25/May/19 3:42 AM",
+          "updated": "25/May/19 3:42 AM",
+          "objectCount": 0,
+          "objectTypeCount": 0
+        }`
+
+				It("schema exists", func() {
+					httpmock.RegisterResponder("GET", endpoint, InsightApiResponder(200, fixture))
+
+					schema, err := testClient.GetObjectSchemaById("5")
+
+					Expect(err).To(BeNil())
+					Expect(schema).To(Not(BeNil()))
+					Expect(schema.Name).To(Equal("Computers"))
+					Expect(schema.Key).To(Equal("IT"))
+					Expect(schema.Description).To(Equal("The IT department schema"))
+				})
+			})
+
+			Context("id does not exist", func() {
+				fixture := `{
+          "errorMessages": [
+              "NotFoundInsightException: Could not find SchemaObject with id: 5"
+          ],
+          "errors": {}
+        }`
+
+				It("returns an error", func() {
+					httpmock.RegisterResponder("GET", endpoint, InsightApiResponder(404, fixture))
+
+					_, err := testClient.GetObjectSchemaById("5")
+
+					Expect(err).To(Not(BeNil()))
+					clientError := insight.ClientError{}
+					Expect(err).To(BeAssignableToTypeOf(&clientError))
+					Expect(err.Error()).To(Equal(fixture))
+				})
+			})
+		})
+
+		Describe("Delete one ObjectSchema by key", func() {
+			listSchemasResponse := `
+      {
+        "objectschemas": [
+           {
+              "id": 3,
+              "name": "Hardware",
+              "objectSchemaKey": "HARD",
+              "status": "Ok",
+              "created": "06/May/19 6:33 AM",
+              "updated": "06/May/19 6:33 AM",
+              "objectCount": 0,
+              "objectTypeCount": 0
+           },
+           {
+             "id": 5,
+             "name": "Computers",
+             "objectSchemaKey": "IT",
+             "status": "Ok",
+             "description": "The IT department schema",
+             "created": "25/May/19 3:42 AM",
+             "updated": "25/May/19 3:42 AM",
+             "objectCount": 0,
+             "objectTypeCount": 0
+          }
+        ]
+      }`
+
+			Context("Schema exists", func() {
+				It("returns no error", func() {
+					httpmock.RegisterResponder("GET", "/rest/insight/1.0/objectschema/list", InsightApiResponder(200, listSchemasResponse))
+					httpmock.RegisterResponder("DELETE", "/rest/insight/1.0/objectschema/5", InsightApiResponder(200, ""))
+
+					err := testClient.DeleteSchemaByKey("IT")
+
+					Expect(err).To(BeNil())
+				})
+			})
+
+			Context("id does not exist", func() {
+				notFoundResponse := `{
+          "errorMessages": [
+              "NotFoundInsightException: Could not find SchemaObject with key: ITX"
+          ],
+          "errors": {}
+        }`
+
+				It("returns an error", func() {
+					httpmock.RegisterResponder("GET", "/rest/insight/1.0/objectschema/list", InsightApiResponder(200, listSchemasResponse))
+					httpmock.RegisterResponder("DELETE", "/rest/insight/1.0/objectschema/5", InsightApiResponder(404, notFoundResponse))
+
+					err := testClient.DeleteSchemaByKey("ITX")
+
+					Expect(err).To(Not(BeNil()))
+					clientError := insight.ObjectSchemaNotFoundError{}
+					Expect(err).To(BeAssignableToTypeOf(&clientError))
+					Expect(err.Error()).To(Equal("Did not find schema 'ITX'\n\nAre you looking for one of these schemas? [HARD IT]\n"))
+				})
+			})
+		})
+
+		Describe("Delete one ObjectSchema", func() {
+			endpoint := MockURLFor("/rest/insight/1.0/objectschema/5")
+
+			Context("Schema exists", func() {
+				It("returns no error", func() {
+					httpmock.RegisterResponder("DELETE", endpoint, InsightApiResponder(200, ""))
+
+					err := testClient.DeleteSchema("5")
+
+					Expect(err).To(BeNil())
+				})
+			})
+
+			Context("id does not exist", func() {
+				fixture := `{
+          "errorMessages": [
+              "NotFoundInsightException: Could not find SchemaObject with id: 5"
+          ],
+          "errors": {}
+        }`
+
+				It("returns an error", func() {
+					httpmock.RegisterResponder("DELETE", endpoint, InsightApiResponder(404, fixture))
+
+					err := testClient.DeleteSchema("5")
+
+					Expect(err).To(Not(BeNil()))
+					clientError := insight.ClientError{}
+					Expect(err).To(BeAssignableToTypeOf(&clientError))
+					Expect(err.Error()).To(Equal(fixture))
+				})
+			})
+		})
+
+		Describe("Update one ObjectSchema", func() {
+			endpoint := "/rest/insight/1.0/objectschema/5"
+			schemaUpdate := insight.ObjectSchemaCreateUpdateRequest{
+				Name:        "Computers",
+				Key:         "IT",
+				Description: "The IT department schema",
+			}
+
+			Context("Successful", func() {
+				fixture := `{
+          "id": 5,
+          "name": "Computers",
+          "objectSchemaKey": "IT",
+          "status": "Ok",
+          "description": "The IT department schema",
+          "created": "25/May/19 3:42 AM",
+          "updated": "25/May/19 3:42 AM",
+          "objectCount": 0,
+          "objectTypeCount": 0
+        }`
+
+				It("completes successfully", func() {
+					httpmock.RegisterResponder("PUT", endpoint, InsightApiResponder(201, fixture))
+
+					updatedSchema, err := testClient.UpdateSchema("5", &schemaUpdate)
+
+					Expect(err).To(BeNil())
+					Expect(updatedSchema).To(Not(BeNil()))
+					Expect(updatedSchema.Name).To(Equal(schemaUpdate.Name))
+					Expect(updatedSchema.Key).To(Equal(schemaUpdate.Key))
+					Expect(updatedSchema.Description).To(Equal(schemaUpdate.Description))
+				})
+			})
+
+			Context("Name does not exist", func() {
+				fixture := `{
+          "errorMessages": [],
+          "errors": {
+            "name": "Name has to be unique!"
+          }
+        }`
+
+				It("returns an error", func() {
+					httpmock.RegisterResponder("PUT", endpoint, InsightApiResponder(404, fixture))
+
+					_, err := testClient.UpdateSchema("5", &schemaUpdate)
+
+					Expect(err).To(Not(BeNil()))
+					clientError := insight.ClientError{}
+					Expect(err).To(BeAssignableToTypeOf(&clientError))
+					Expect(err.Error()).To(Equal(fixture))
+				})
+			})
+		})
+	})
+})
